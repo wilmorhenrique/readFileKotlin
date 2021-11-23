66765a3e74ce38a464dd272f72d51aaba056624e
whneto <Henrique76>
2021-11-23 20:25:25 +0000

    Refactor function extractFields

--- src/main/java/pt/whd/readFile/App.kt
+++ src/main/java/pt/whd/readFile/App.kt
@@ -3,45 +3,53 @@ package pt.whd.readFile
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
-import java.io.IOException
 import java.util.stream.Collectors
 
-/**
- * Hello world!
- *
- */
 
 fun main(args: Array<String>) {
     readFile(args[0])
 }
 
-@Throws(IOException::class)
-fun readFile(fileName: String?): String {
+
+fun readFile(fileName: String): String {
     val file = File(fileName)
     val br = BufferedReader(FileReader(file))
     val ids = StringBuffer()
-    val pedidos: MutableList<Pedido> = ArrayList()
-    val text:List<String> = br.readLines()
-    for(fileLine in text){
-        val fields = fileLine.split(";").toTypedArray()
-        if (isValidLine(fields)) {
-            ids.append(fields[0]).append(",")
-            val pedido = Pedido(java.lang.Long.valueOf(fields[0]), fields[1], getDouble(fields[2]))
-            pedidos.add(pedido)
-        }
-    }
+
+    val pedidos: MutableList<Pedido> = extractFields(br)
     br.close()
     println("Resumo do ficheiro")
     println("\n")
     println("Total de Registos: " + pedidos.size)
     println("\n")
+
+    pedidos.forEach {
+        ids.append(it.id).append(",")
+    }
+
     val pedidosPorTipo = pedidos.stream().collect(Collectors.groupingBy(Pedido::estado))
+
     pedidosPorTipo.forEach { (k: String, v: List<Pedido>) ->
         println("$k:${v.size} soma:${v.stream().collect(Collectors.summingDouble(Pedido::valor))}")
+
     }
+
     println("\n")
     println("IDs: " + ids.substring(0, ids.length - 1))
     return ids.substring(0, ids.length - 1)
+}
+
+private fun extractFields(br: BufferedReader): MutableList<Pedido> {
+    val pedidos: MutableList<Pedido> = ArrayList()
+    val text: List<String> = br.readLines()
+    for (fileLine in text) {
+        val fields = fileLine.split(";").toTypedArray()
+        if (isValidLine(fields)) {
+            val pedido = Pedido(fields[0].toLong(), fields[1], getDouble(fields[2]))
+            pedidos.add(pedido)
+        }
+    }
+    return pedidos
 }
 
 private fun getDouble(valor: String?): Double {
